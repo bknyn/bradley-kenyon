6504aff3600985ee4267afa77d169dcd9b99fb4d
Bradley Kenyon <b@bradleykenyon.com>
2020-06-28 13:33:02 -0700

    add css variables and component to write to root

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,14 +4,16 @@ import Document, {
   Main,
   NextScript
 } from 'next/document';
+import RootCSSVars from '../components/rootCSSVars';
 
 class CustomDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
-          <link href="https://use.typekit.net/kna4wmf.css" rel="stylesheet" />
           <link rel="icon" href="/favicon.ico" />
+          <link href="https://use.typekit.net/kna4wmf.css" rel="stylesheet" />
+          <RootCSSVars />
         </Head>
         <body>
           <Main />
---
+++ src/styles/variables.js
@@ -0,0 +1,12 @@
+export const colors = {
+  primaryColor: '#0033a6',
+  bgColor: '#fff',
+  altBgColor: '#f5f5f5',
+  darkGrey: '#333',
+  lightGrey: '#999'
+};
+
+export const spacing = {
+  spacer: 8,
+  get gridGutter() { return this.spacer * 2; }
+};
